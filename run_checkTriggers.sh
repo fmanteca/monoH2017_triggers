83da769056952956f06f6c7cd7d84a748926ada0
fmanteca <fmanteca@ifca.unican.es>
2018-12-17 11:14:10 +0100

    updated

--- run_checkTriggers.sh
+++ run_checkTriggers.sh
@@ -1,1 +1,4 @@
-root -l -q -b "checkTriggers.C(\"nanoLatino_ZprimeToMuMu_M-5000__part0.root\")"
+root -l -q -b "checkTriggers.C(\"nanoLatino_monoH_2HDM_MZp-400_MA0-300\")"
+root -l -q -b "checkTriggers.C(\"nanoLatino_monoH_2HDM_MZp-600_MA0-300\")"
+root -l -q -b "checkTriggers.C(\"nanoLatino_monoH_2HDM_MZp-1000_MA0-300\")"
+root -l -q -b "checkTriggers.C(\"nanoLatino_monoH_2HDM_MZp-1700_MA0-300\")"
